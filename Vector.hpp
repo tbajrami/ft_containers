c450f65fabbf3be418fb72f17efc4da9e3a9cea5
Ted Bajrami <tbajrami@student.42lyon.fr>
2021-03-04 14:41:06 +0100

    g

--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tbajrami <tbajrami@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 11:47:28 by tbajrami          #+#    #+#             */
-/*   Updated: 2021/03/02 12:42:26 by tbajrami         ###   ########lyon.fr   */
+/*   Updated: 2021/03/04 14:40:46 by tbajrami         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,23 +43,37 @@ protected:
 
 public:
 
+    class iterator
+    {
+    public:
+        value_type   *_ptr;
+    };
 
+    Vector::iterator begin()
+    {
+        Vector::iterator it;
+        it._ptr = this->_begin;
+    }
+
+    Vector::iterator end()
+    {
+        Vector::iterator it;
+        it._ptr = this->_begin;
+    }
 
     explicit Vector() : _size(0), _capacity(0), _begin(NULL), _end(NULL), _end_cap(NULL) {};
 
     explicit Vector(size_t n, const value_type &val)
     {
-        size_t j;
         size_t i;
 
-        for (j = 0 ; pow(2, j) < n ; j++);
-        int k = pow(2, j);
         this->_size = n;
-        this->_capacity = k;
-        this->_begin = new value_type[k];
+        this->_capacity = n;
+        this->_begin = new value_type[n];
         for (i = 0 ; i < n ; i++)
             this->_begin[i] = val;
         this->_end = this->_begin + n;
+        this->_end_cap = this->_begin + n;
     }
 
     ~Vector() {delete [] this->_begin;}
@@ -72,21 +86,35 @@ public:
     size_t size() const {return this->_size;}
     size_t max_size() const {return std::numeric_limits<size_t>::max() / sizeof(value_type);}
     size_t capacity() const {return this->_capacity;}
+    bool   empty() const {return !this->_size;}
 
     void resize(size_t n, value_type val = value_type())
     {
         if (n == this->_size) return ;
         
-        value_type *new_vec = new value_type[n];
+        if (n < this->_capacity);
+        else if (n <= this->_capacity * 2)
+            this->_capacity *= 2;
+        else if (n > this->_capacity * 2)
+            this->_capacity = n;
+
+        value_type *new_vec = new value_type[this->_capacity];
         
         for (size_t i = 0 ; i < n ; i++)
             new_vec[i] = (i < this->_size) ? this->at(i) : val;
         delete [] this->_begin;
         this->_begin = new_vec;
         this->_end = this->_begin + n;
-        this->_end_cap = this->_begin + n;
+        this->_end_cap = this->_begin + this->_capacity;
         this->_size = n;
+    }
+
+    void reserve(size_t n)
+    {
+        if (n <= this->_capacity) return ;
+        if (n > this->max_size()) throw std::length_error("");
         this->_capacity = n;
+        this->_end_cap = this->_begin + n;
     }
 
     /* ELEMENT ACCESS */
@@ -97,6 +125,8 @@ public:
             throw std::out_of_range("");
         return *(this->_begin + n);
     }
+
+    value_type &operator[](size_t n) {return at(n);}
 };
 
 }
--- main.cpp
+++ main.cpp
@@ -6,57 +6,156 @@
 /*   By: tbajrami <tbajrami@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 08:34:32 by tbajrami          #+#    #+#             */
-/*   Updated: 2021/03/02 12:30:23 by tbajrami         ###   ########lyon.fr   */
+/*   Updated: 2021/03/04 13:35:53 by tbajrami         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Vector.hpp"
 #include <vector>
+#include <list>
 
 int main()
 {
     std::vector<int> c(10, 0);
     ft::Vector<int> b(10, 0);
 
+    std::cout << "\n######### --------- VECTOR --------- #########\n\n";
+    std::cout << "\n##### ----- SIZE FUNCTIONS ----- #####\n";
+
+    std::cout << "\n### --- vector(10, 0) then assign value 1 to 10 --- ###\n";
+
     for (size_t i = 0 ; i < b.size() ; i++)
         b.at(i) = i + 1;
     for (size_t j = 0 ; j < c.size() ; j++)
         c.at(j) = j + 1;
-        
-    std::cout << "ft::Vector: ";
-    for (size_t i = 0 ; i < b.size() ; i++)
-        std::cout << ' ' << b.at(i);
-    std::cout << '\n';
 
-    std::cout << "std::vector: ";
-    for (size_t i = 0 ; i < c.size() ; i++)
-        std::cout << ' ' << c.at(i);
-    std::cout << '\n';
-
-    b.resize(5);
-    b.resize(8,100);
-    b.resize(12);
-
-    c.resize(5);
-    c.resize(8,100);
-    c.resize(12);
-
-    std::cout << "ft::Vector: ";
-    for (size_t i = 0 ; i < b.size() ; i++)
-        std::cout << ' ' << b.at(i);
-    std::cout << '\n';
-
-    std::cout << "std::vector: ";
-    for (size_t i = 0 ; i < c.size() ; i++)
-        std::cout << ' ' << c.at(i);
-    std::cout << '\n';
-    
     std::cout << "\nft vector size: " << b.size() << std::endl;
-    std::cout << "std vector size: " << c.size() << std::endl;
-    
-    std::cout << "\nft vector max_size: " << b.max_size() << std::endl;
-    std::cout << "std vector max_size: " << c.max_size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
 
     std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
-    std::cout << "std vector capacity: " << c.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
+
+    std::cout << "\nft vector: ";
+    for (size_t i = 0 ; i < b.size() ; i++)
+        std::cout << ' ' << b.at(i);
+    std::cout << '\n';
+
+    std::cout << "sd vector: ";
+    for (size_t i = 0 ; i < c.size() ; i++)
+        std::cout << ' ' << c.at(i);
+    std::cout << '\n';
+
+    std::cout << "\n### --- resize(5) --- ###\n";
+
+    b.resize(5);
+    c.resize(5);
+
+    std::cout << "\nft vector size: " << b.size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
+
+    std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
+
+    std::cout << "\nft vector: ";
+    for (size_t i = 0 ; i < b.size() ; i++)
+        std::cout << ' ' << b.at(i);
+    std::cout << '\n';
+
+    std::cout << "sd vector: ";
+    for (size_t i = 0 ; i < c.size() ; i++)
+        std::cout << ' ' << c.at(i);
+    std::cout << '\n';
+    
+    std::cout << "\n### --- resize(8, 100) --- ###\n";
+
+    b.resize(8,100);
+    c.resize(8,100);
+
+    std::cout << "\nft vector size: " << b.size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
+
+    std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
+    
+    std::cout << "\nft vector: ";
+    for (size_t i = 0 ; i < b.size() ; i++)
+        std::cout << ' ' << b.at(i);
+    std::cout << '\n';
+
+    std::cout << "sd vector: ";
+    for (size_t i = 0 ; i < c.size() ; i++)
+        std::cout << ' ' << c.at(i);
+    std::cout << '\n';
+    
+    std::cout << "\n### --- resize(12) --- ###\n";
+
+    b.resize(12);
+    c.resize(12);
+
+    std::cout << "\nft vector size: " << b.size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
+
+    std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
+
+    std::cout << "\nft vector: ";
+    for (size_t i = 0 ; i < b.size() ; i++)
+        std::cout << ' ' << b.at(i);
+    std::cout << '\n';
+
+    std::cout << "sd vector: ";
+    for (size_t i = 0 ; i < c.size() ; i++)
+        std::cout << ' ' << c.at(i);
+    std::cout << '\n';
+
+    std::cout << "\n### --- max size --- ###\n";
+    
+    std::cout << "\nft vector max_size: " << b.max_size() << std::endl;
+    std::cout << "sd vector max_size: " << c.max_size() << std::endl;
+
+    std::cout << "\n### --- empty ? --- ###\n";
+
+    std::cout << "\nprevious vectors :";
+    std::cout << "\nft vector empty ? " << b.empty() << std::endl;
+    std::cout << "sd vector empty ? " << c.empty() << std::endl;
+
+    ft::Vector<int> d;
+    std::vector<int> e;
+    
+    std::cout << "\nnew vectors without parameters :\n";
+    std::cout << "ft vector empty ? " << d.empty() << std::endl;
+    std::cout << "sd vector empty ? " << e.empty() << std::endl;
+
+    std::cout << "\n### --- reserve(10) --- ###\n";
+
+    b.reserve(10);
+    c.reserve(10);
+
+    std::cout << "\nft vector size: " << b.size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
+    
+    std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
+
+    std::cout << "\n### --- reserve(42) --- ###\n";
+
+    b.reserve(42);
+    c.reserve(42);
+
+    std::cout << "\nft vector size: " << b.size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
+
+    std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
+
+    std::cout << "\n### --- resize(100) --- ###\n";
+
+    b.resize(100);
+    c.resize(100);
+
+    std::cout << "\nft vector size: " << b.size() << std::endl;
+    std::cout << "sd vector size: " << c.size() << std::endl;
+
+    std::cout << "\nft vector capacity: " << b.capacity() << std::endl;
+    std::cout << "sd vector capacity: " << c.capacity() << std::endl;
 }
